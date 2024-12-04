865a1a648f9e55dee6ac1e44e02768347d607bff
Alpha Particle <Resonanz@users.noreply.github.com>
2024-12-04 13:57:08 +1300

    Looking great !!!

--- src/defs_and_consts.rs
+++ src/defs_and_consts.rs
@@ -1,10 +1,1 @@
-use egui::Vec2;
 
-pub const FRAME_SIZE: Vec2 = Vec2 { x: 100.0, y: 60.0 };
-pub const ICON_SIZE: Vec2 = Vec2 { x: 40.0, y: 40.0 };
-pub const TEXT_SIZE: Vec2 = Vec2 { x: 100.0, y: 20.0 };
-pub const WIDGET_SPACE_ABOVE: f32 = 20.0;
-pub const WIDGET_SPACE_BELOW: f32 = 10.0;
-pub const ICON_TEXT_SPACE: f32 = 4.0;
-
-pub const DEFAULT_TEXT: &'static str = "Need Text";
